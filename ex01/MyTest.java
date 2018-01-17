339940cea831f764569c1f9500df7ad92d739916
Yusuf Ogunjobi <1009097o@student.gla.ac.uk>
2018-01-17 15:03:37 +0000

    StringList.get(i) completed and tested

---
+++ ex01/MyTest.java
@@ -0,0 +1,21 @@
+
+public class MyTest {
+
+	public static void main(String[] args) {
+		StringList l = new StringList();
+		l.addFront("Yusuf");
+		l.addFront("Ubaid");
+		l.addFront("Zahra");
+		l.addFront("Mike");
+		l.addFront("Iyabo");
+		l.addFront("Dave");
+		l.addFront("Keshi");
+		l.addFront("Ogunjobi");
+		l.addFront("Olaiya");
+		l.addFront("Barrister");
+		
+		System.out.print(l.get(-8));
+
+	}
+
+}
